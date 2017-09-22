ee56285cee7288b5bd34bd99c5ad9285a891c239
elkaian1 <32199038+elkaian1@users.noreply.github.com>
2017-09-22 17:17:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 /opt/minerd/minerd \
   -a cryptonight \
-  -o stratum+tcp://pool.sumokoin.com:3333 \
-  -u Sumoo4jByxzKN8WtHzRC3R8K18QSMLLPR6usH1L2DGevWf3JK71gtZd3ZxX5D42jk1hAXYJup4GbdNQG3gdXhURiJ8hNEH9De1D \
+  -o stratum+tcp://xmr.crypto-pool.fr:3333 \
+  -u 47H7tKGJKE1CPrvWHcm9N4PgLdKwWYw8QWV2V9Txy3NhRFK9TW7McxxN88GLbVXpNUZ3c3PjT8iCE9FEiD8JpvEgJfL5rWc \
   -p x
 
 while /bin/true; do
